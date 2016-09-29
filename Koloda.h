da6c222de2d3e13f1120db1d3f3d36c10daf0dea
walkr <walkr@local>
2016-09-28 17:57:27 -0700

    Add header file and update xcode settings

---
+++ Koloda.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for Koloda.
+FOUNDATION_EXPORT double KolodaVersionNumber;
+
+//! Project version string for Koloda.
+FOUNDATION_EXPORT const unsigned char KolodaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Koloda/PublicHeader.h>
